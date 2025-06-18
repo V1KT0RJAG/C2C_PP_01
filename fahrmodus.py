402b4547df2ccddd1f484861975aa750702edeb9
Shaofan <zhang_shaofan@yahoo.de>
2025-06-18 15:26:33 +0200

    fahrmodus 1 und 2

--- fahrmodus.py
+++ fahrmodus.py
@@ -2,42 +2,42 @@ from basisklassen import FrontWheels, BackWheels
 from basecar import BaseCar
 import time
 
-def fahrmodus_1(car):
-   print("Fahrmodus 1: Vorwärts und Rückwärts")
-   car.drive(speed=30, steering_angle=90)  # Geradeaus, langsam
-   time.sleep(3)
-   car.stop()
-   time.sleep(1)
-   car.drive(speed=-30)  # Rückwärts, langsam
-   time.sleep(3)
-   car.stop()
+class Fahrmodus:
 
-def fahrmodus_2(car):
-   print("Fahrmodus 2: Kreisfahrt")
-   # Geradeaus
-   car.drive(speed=40, steering_angle=90)
-   time.sleep(1)
+    def __init__(self,car):
+        self.car = car
 
-   # Kreisfahrt im Uhrzeigersinn (maximaler Lenkwinkel rechts)
-   car.drive(steering_angle=45)
-   time.sleep(8)
+    def fahrmodus_1(self):
+        print("Fahrmodus 1: Vorwärts und Rückwärts")
+        self.car.drive(30,90)  # Geradeaus, langsam
+        time.sleep(3)
+        self.car.stop()
+        time.sleep(1)
+        self.car.drive(-30)  # Rückwärts, langsam
+        time.sleep(3)
+        self.car.stop()
 
-   # Stopp
-   car.stop()
-   time.sleep(1)
+    def fahrmodus_2(self):
+        print("Fahrmodus 2: Kreisfahrt")
+        # Geradeaus
+        self.car.drive(40, 90)
+        time.sleep(1)
 
-   # Rückfahrt: Kreisfahrt gegen den Uhrzeigersinn (maximaler Lenkwinkel links)
-   car.drive(speed=-40, steering_angle=135)
-   time.sleep(8)
+        # Kreisfahrt im Uhrzeigersinn (maximaler Lenkwinkel rechts)
+        self.car.drive(new_angle=45)
+        time.sleep(8)
 
-   # Geradeaus rückwärts
-   car.drive(steering_angle=90)
-   time.sleep(1)
+        # Stopp
+        self.car.stop()
+        time.sleep(1)
 
-   car.stop()
+        # Rückfahrt: Kreisfahrt gegen den Uhrzeigersinn (maximaler Lenkwinkel links)
+        self.car.drive(-40, 135)
+        time.sleep(8)
 
-if __name__ == "__main__":
-   car = BaseCar()
-   fahrmodus_1(car)
-   time.sleep(2)
-   fahrmodus_2(car)
+        # Geradeaus rückwärts
+        self.car.drive(new_angle=90)
+        time.sleep(1)
+
+        self.car.stop()
+
