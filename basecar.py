d53e8479efa56857b0aaabb91136beae80738a45
Janik <janik.kelm@volkswagen.de>
2025-06-18 11:01:48 +0200

    basecar

--- basecar.py
+++ basecar.py
@@ -2,6 +2,8 @@ import basisklassen
 import time
 import json
 
+
+
 class BaseCar:
 
     def __init__(self, front, back):
@@ -10,11 +12,12 @@ class BaseCar:
         self.__direction = 0
         self.front = front
         self.back = back
+        self.offset_config()
         print("BaseCar erzeugt")
         
     @property
     def steering_angle(self):
-        print(self.__steering_angle)
+        # print(self.__steering_angle)
         return self.__steering_angle
     @steering_angle.setter
     def steering_angle(self, angle):
@@ -27,7 +30,7 @@ class BaseCar:
 
     @property
     def speed(self):
-        print(self.__speed)
+        # print(self.__speed)
         return self.__speed
     @speed.setter
     def speed(self, new_speed):
@@ -43,63 +46,70 @@ class BaseCar:
         print(self.__direction)
         return self.__direction
 
-    def drive(self, new_speed, new_angle):
+    def drive(self, new_speed: int = speed, new_angle: int = steering_angle):                          # Falls nur ein Parameter gesetzt wird, soll der momentan aktuelle Wert des fehlenden Parameters beibehalten werden.
         self.speed = new_speed
         self.steering_angle = new_angle
         print(f"Geschwindigkeit von {self.speed} und Lenkwinkel von {self.steering_angle} wurde übermittelt")
         time.sleep(1)
-
-        if self.getSpeed >= 0:
-            self.BW.forward()
-            self.BW.left_wheel.speed = self.getSpeed
-            self.BW.right_wheel.speed = self.getSpeed
-        elif self.getSpeed < 0:
-            self.BW.backward()              
-            self.BW.left_wheel.speed = abs(self.getSpeed)
-            self.BW.right_wheel.speed = abs(self.getSpeed)
+        # print(self.steering_angle)
+        # print(type(self.steering_angle))
+        self.front.turn(self.steering_angle)
+     
+        if self.speed >= 0:
+            self.back.forward()
+            self.back.left_wheel.speed = self.speed
+            self.back.right_wheel.speed = self.speed
+        elif self.speed < 0:
+            self.back.backward()              
+            self.back.left_wheel.speed = abs(self.speed)
+            self.back.right_wheel.speed = abs(self.speed)
          
 
-        fw.turn(new_angle)
-        print(new_angle)
-        print(type(new_angle))
+
 
     def stop(self):
         self.speed = 0
+        self.back.left_wheel.speed = self.speed
+        self.back.right_wheel.speed = self.speed
 
-    def read_config(self, path):
+    def offset_config(self):
         try:
             with open("config.json", "r") as f:
                 data = json.load(f)
-                turning_offset = data["turning_offset"]
-                forward_A = data["forward_A"]
-                forward_B = data["forward_B"]
+                turning_offset = data.get("turning_offset", 0)
+                forward_A = data.get("forward_A", 0)
+                forward_B = data.get("forward_B", 0)
                 print("Daten in config.json:")
                 print(" - Turning Offset: ", turning_offset)
                 print(" - Forward A: ", forward_A)
                 print(" - Forward B: ", forward_B)
         except:
             print("Keine geeignete Datei config.json gefunden!")
+            car.stop()
         else:
-            print("Test der Vorderräder:")
-            x = input(' Drücken Sie ENTER zum Start.')
-            self.front(turning_offset=turning_offset)
-            self.front.test()
-            time.sleep(1)
-            print("Test der Hinterräder:")
-            x = input(' ACHTUNG! Das Auto wird ein Stück fahren!\n Drücken Sie ENTER zum Start.')
-            self.back(forward_A=forward_A, forward_B=forward_B)
-            self.back.test()
+            print("Offset wurde geschrieben")
+            self.front._turning_offset = turning_offset
+            self.back.forward_A = forward_A
+            self.back.forward_B = forward_B
+        finally:
+            pass
 
-fw = basisklassen.FrontWheels()
-bw = basisklassen.BackWheels() 
 
-car = BaseCar(fw, bw)
-car.steering_angle = 140
-car.steering_angle
-car.speed =-130
-car.speed
-car.speed = 80
-car.speed
+#           so würde es auch gehen nur dann müsste man bei der erstellung nur fw = FrontWheels schreiben ohne () weil erst in dem Code die "Räder" angebaut werden           
+#            FrontWheels(turning_offset=turning_offset) 
+#            BackWheeels(forward_A=forward_A, forward_B=forward_B)
 
-#Auto wird gestopptAdd commentMore actions
-car.stop()# from basisklassen import Ultrasonic, BackWheels, FrontWheels
+
+# fw = basisklassen.FrontWheels()
+# bw = basisklassen.BackWheels() 
+
+# car = BaseCar(fw, bw)
+# car.steering_angle = 140
+# car.steering_angle
+# car.speed =-130
+# car.speed
+# car.speed = 80
+# car.speed
+
+# #Auto wird gestopptAdd commentMore actions
+# car.stop()# from basisklassen import Ultrasonic, BackWheels, FrontWheels
