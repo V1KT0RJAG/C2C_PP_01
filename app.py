e5a2a1b57b68e8e0e3b00937f523c2a2da956ffb
V1KT0RJAG <vaguilarg0500@egresado.ipn.mx>
2025-06-24 14:37:12 +0200

    test plot lines

--- app.py
+++ app.py
@@ -1,30 +1,40 @@
 from dash import html, dcc, Dash, Input, Output
 import plotly.express as px
+import carmain
 
-df = px.data.gapminder().query("year == 2007")
+import pandas as pd
+import numpy as np
 
-initial_fig = px.scatter(df, x="gdpPercap", y="lifeExp")
+# Load the CSV file
+#df = pd.read_csv('simulated_car_data.csv', delimiter=',')
+df 
 
-app = Dash(__name__)
+print(df.head(10))
 
-app.layout = html.Div(
-    [
-    html.H1("Einführung in Dash"),
-    html.Button(),
-    dcc.Graph(id="my-first-graph", figure=initial_fig),
-    dcc.Dropdown(id="my-first-fropdown",options=list(df["continent"].unique()), value="Asia")
-    ]
-)
+#df = px.data.gapminder()
+fig = px.line(log_df, x="time", y="speed")
+fig.show()
 
-@app.callback(
-    Output("my-first-graph", "figure"),
-    Input("my-first-fropdown", "value")
-)
-def update_first_graph(dropdown_value):
-    filtered = df.query("continent == @dropdown_value")
-    figure = px.scatter(filtered, x="gdpPercap", y="lifeExp")
+# app = Dash(__name__)
 
-    return figure
+# app.layout = html.Div(
+#     [
+#     html.H1("Einführung in Dash"),
+#     #html.Button(),
+#     dcc.Graph(id="my-first-graph", figure=initial_fig),
+#     #dcc.Dropdown(id="my-first-fropdown",options=list(df["speed"].unique()), value="Asia")
+#     ]
+# )
 
-if __name__ == "__main__":
-    app.run_server(debug=True)
+# @app.callback(
+#     Output("my-first-graph", "figure"),
+#     Input("my-first-fropdown", "value")
+# )
+# def update_first_graph(dropdown_value):
+#     #filtered = df.query("speed == @dropdown_value")
+#     #figure = px.scatter(filtered, x="time", y="speed")
+
+#     return figure
+
+# if __name__ == "__main__":
+#     app.run_server(debug=True)
