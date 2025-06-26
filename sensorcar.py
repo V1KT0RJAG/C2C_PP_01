e69d4e888274fd9bf429dcac9a2569dbb16b5341
Shaofan <zhang_shaofan@yahoo.de>
2025-06-26 11:58:27 +0200

    1st ver not working

--- sensorcar.py
+++ sensorcar.py
@@ -36,6 +36,7 @@ class SensorCar(BaseCar):
 
     def get_ir(self):
         #Logik aufbauen
+<<<<<<< HEAD
         self.analog = self.infra.read_analog() 
         self.digital = self.infra.read_digital()
         print(self.analog, self.digital)
@@ -60,6 +61,11 @@ class SensorCar(BaseCar):
             ff.close()
         finally:
             pass
+=======
+        data_analog = self.infra.read_analog() 
+        data_digital = self.infra.read_digital()
+        print(data_analog, data_digital)
+>>>>>>> f816594 (1st ver not working)
 
 fw = FrontWheels()
 bw = BackWheels() 
@@ -68,6 +74,7 @@ ir = Infrared()
 sc = SensorCar(fw, bw, ultra=usm, infra=ir)
 
 
+<<<<<<< HEAD
 fm = Fahrmodus(sc)
 
 #sc.get_distance()
@@ -149,3 +156,54 @@ while True:
 
 sc.drive(new_speed=0, new_angle=90)
 sc.stop()"""
+=======
+analog = ir.read_analog()
+digital = ir.read_digital()
+print(f"Analog: {analog} | Digital: {digital}")
+ir.cali_references()
+ir.set_references
+time.sleep(5)
+
+#fahrmodus 5
+#linie vefolgen
+print("Fahrmodus 5:")
+start_time = time.time()
+sc.drive(new_speed=40, new_angle=90)
+
+drive_duration = 0
+
+while drive_duration<20:
+    #[1,0,0,0,0]
+    if digital[0] == 1:
+        sc.drive(new_speed=40, new_angle=45)
+        print(digital)
+        print(analog)
+    #[0,1,0,0,0]
+    if digital[1] == 1:
+        sc.drive(new_speed=40, new_angle=60)
+        print(digital)
+        print(analog)
+    #[0,0,1,0,0]
+    if digital[2] == 1:
+        sc.drive(new_speed=40, new_angle=90)
+        print(digital)
+        print(analog)
+    #[0,0,0,1,0]
+    if digital[3] == 1:
+        sc.drive(new_speed=40, new_angle=120)
+        print(digital)
+        print(analog)
+    #[0,0,0,0,1]
+    if digital[4] == 1:
+        sc.drive(new_speed=40, new_angle=135)
+        print(digital)
+        print(analog)
+    #[0,0,0,0,0], [1,1,1,1,1], [1,1,0,0,0]
+    else:
+        sc.stop()
+        print(digital)
+        print(analog)
+    drive_duration += 1
+
+sc.stop()
+>>>>>>> f816594 (1st ver not working)
