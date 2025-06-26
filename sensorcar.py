f816594d3bb2cf8887093c8cf48b68ab22ba5d86
Shaofan <zhang_shaofan@yahoo.de>
2025-06-26 11:58:27 +0200

    1st ver not working

--- sensorcar.py
+++ sensorcar.py
@@ -28,10 +28,12 @@ class SensorCar(BaseCar):
         print("[Fehler] Keine gültige Distanzmessung möglich.") 
         #self.stop()  
         return 0
-    
-    # def get_ir(self):
-    #     data = self.infra.read_analog() 
-    #     print(data)
+
+    def get_ir(self):
+        #Logik aufbauen
+        data_analog = self.infra.read_analog() 
+        data_digital = self.infra.read_digital()
+        print(data_analog, data_digital)
 
 fw = FrontWheels()
 bw = BackWheels() 
@@ -46,3 +48,48 @@ analog = ir.read_analog()
 digital = ir.read_digital()
 print(f"Analog: {analog} | Digital: {digital}")
 ir.cali_references()
+ir.set_references
+time.sleep(5)
+
+#fahrmodus 5
+#linie vefolgen
+print("Fahrmodus 5:")
+start_time = time.time()
+sc.drive(new_speed=40, new_angle=90)
+
+drive_duration = 0
+
+while drive_duration<20:
+    #[1,0,0,0,0]
+    if digital[0] == 1:
+        sc.drive(new_speed=40, new_angle=45)
+        print(digital)
+        print(analog)
+    #[0,1,0,0,0]
+    if digital[1] == 1:
+        sc.drive(new_speed=40, new_angle=60)
+        print(digital)
+        print(analog)
+    #[0,0,1,0,0]
+    if digital[2] == 1:
+        sc.drive(new_speed=40, new_angle=90)
+        print(digital)
+        print(analog)
+    #[0,0,0,1,0]
+    if digital[3] == 1:
+        sc.drive(new_speed=40, new_angle=120)
+        print(digital)
+        print(analog)
+    #[0,0,0,0,1]
+    if digital[4] == 1:
+        sc.drive(new_speed=40, new_angle=135)
+        print(digital)
+        print(analog)
+    #[0,0,0,0,0], [1,1,1,1,1], [1,1,0,0,0]
+    else:
+        sc.stop()
+        print(digital)
+        print(analog)
+    drive_duration += 1
+
+sc.stop()
