03495b0f634a9a4a88ee6078358607694475de59
V1KT0RJAG <vaguilarg0500@egresado.ipn.mx>
2025-06-30 10:45:28 +0200

    App Audi aktualisiert

--- app_audi.py
+++ app_audi.py
@@ -6,11 +6,10 @@ from dash import dcc, html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import pandas as pd
-from datetime import datetime
+import numpy as np
+from datetime import datetime, timedelta
 import ast
 import plotly.express as px
-import subprocess
-from datetime import timedelta
 import  os
 from fahrmodus import Fahrmodus
 
@@ -43,18 +42,23 @@ def calculate_kpis(df):
     #?? negative Geschwindigkeit
     min_speed = df['speed'].min()
     avg_speed = abs(df['speed'].mean())
+    avg_speed_fl =  avg_speed.item() 
     total_time = df['UTCtime'].max() - df['UTCtime'].min()
+    total_time_sec = total_time.item()
+    print(total_time_sec)
     #?? str und runden
     total_time_str = str(timedelta(seconds=total_time))
-    return max_speed, min_speed, avg_speed, total_time_str
- 
+    print(total_time_str)
+    total_distance_calculation = avg_speed_fl * total_time_sec
+    return max_speed, min_speed, avg_speed, total_time_sec, total_distance_calculation
 
-max_speed, min_speed, avg_speed, total_time_str = calculate_kpis(df)
+max_speed, min_speed, avg_speed, total_time_sec, total_distance_calculation = calculate_kpis(df)
+
 df['time'] = pd.to_datetime(df['UTCtime'], unit='s')
 fig = px.line(df, x='time', y='speed', title='Geschwindigkeit über Zeit',
               labels={'time': 'Zeit', 'speed': 'Geschwindigkeit (km/h)'},
               template='plotly_dark')
- 
+
 # URL zum Bootstrap 5.3 Stylesheet – ermöglicht die Nutzung von Bootstrap-Klassen wie 'btn', 'row', 'col', 'text-center' usw.
 BOOTSTRAP = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
  
@@ -74,7 +78,11 @@ app.layout = dbc.Container([
                     {'label': 'Fahrmodus 1', 'value': 1},                    
                     {'label': 'Fahrmodus 2', 'value': 2},
                     {'label': 'Fahrmodus 3', 'value': 3},
-                    {'label': 'Fahrmodus 4', 'value': 4}
+                    {'label': 'Fahrmodus 4', 'value': 4},
+                    {'label': 'Fahrmodus 5', 'value': 5},
+                    {'label': 'Fahrmodus 6', 'value': 6},
+                    {'label': 'Fahrmodus 7', 'value': 7},
+                    {'label': 'Fahrmodus 8', 'value': 8}
                 ],
                 value=4,
                 clearable=False,
@@ -88,19 +96,19 @@ app.layout = dbc.Container([
     dbc.Row([
         dbc.Col(dbc.Card(dbc.CardBody([
             html.H4("🏁 Max Speed"), html.H2(id="max-speed", children=f"{max_speed} km/h")
-        ])), width=3),
+        ])), width=2),
         dbc.Col(dbc.Card(dbc.CardBody([
             html.H4("🐢 Min Speed"), html.H2(id="min-speed", children=f"{min_speed} km/h")
-        ])), width=3),
+        ])), width=2),
         dbc.Col(dbc.Card(dbc.CardBody([
             html.H4("⚖️ Avg Speed"), html.H2(id="avg-speed", children=f"{avg_speed:.2f} km/h")
-        ])), width=3),
-        # dbc.Col(dbc.Card(dbc.CardBody([
-        #     html.H4("🗺️ Total distance"), html.H2(id="total-distance", children=total_distance)
-        # ])), width=3),
+        ])), width=2),
         dbc.Col(dbc.Card(dbc.CardBody([
-            html.H4("⏱️ Total Time"), html.H2(id="total-time", children=total_time_str)
-        ])), width=3),
+            html.H4("🗺️ Total distance"), html.H2(id="total-distance", children=f"{total_distance_calculation} km")
+        ])), width=2),
+        dbc.Col(dbc.Card(dbc.CardBody([
+            html.H4("⏱️ Total Time"), html.H2(id="total-time", children=total_time_sec)
+        ])), width=2),
     ], className="gy-4"),
  
     # Diagramm
@@ -117,7 +125,7 @@ app.layout = dbc.Container([
     Output('min-speed', 'children'),
     Output('avg-speed', 'children'),
     Output('total-time', 'children'),
-    #  Output('total-distance', 'children'),
+    Output('total-distance', 'children'),
     Output('speed-graph', 'figure'),
     Input('start-button', 'n_clicks'),
     Input('mode-dropdown', 'value')
@@ -135,13 +143,13 @@ def update_dashboard(n_clicks, mode):
  
     # Neue Daten laden
     df = pd.read_csv('sonic_log.csv', delimiter=',')
-    max_speed, min_speed, avg_speed, total_time_str = calculate_kpis(df)
+    max_speed, min_speed, avg_speed, total_time_sec, total_distance_calculation = calculate_kpis(df)
     df['time'] = pd.to_datetime(df['UTCtime'], unit='s')
     fig = px.line(df, x='time', y='speed', title='Geschwindigkeit über Zeit',
                   labels={'time': 'Zeit', 'speed': 'Geschwindigkeit (km/h)'},
                   template='plotly_dark')
  
-    return f"{max_speed} km/h", f"{min_speed} km/h", f"{avg_speed:.2f} km/h", total_time_str, fig
+    return f"{max_speed} km/h", f"{min_speed} km/h", f"{avg_speed:.2f} km/h", total_time_sec, total_distance_calculation, fig
  
 if __name__ == '__main__':
     app.run(debug=True)
